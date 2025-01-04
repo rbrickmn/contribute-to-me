3bf290673caa2960433d644483d705881e256ff0
rbrickmn <rbrickmn@gmail.com>
2025-01-04 13:11:01 -0500

    Add properly running test cases

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
+// jest.config.js
 export default {
-  testEnvironment: "jest-environment-jsdom", // Full module name is required
+  testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["@testing-library/jest-dom"],
+  transform: {
+    "^.+\\.[t|j]sx?$": "babel-jest", // Use babel-jest for JSX files
+  },
   moduleNameMapper: {
     "\\.(css|scss|less)$": "identity-obj-proxy", // Mock CSS imports
   },
